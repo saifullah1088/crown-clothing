295d6db838e0980392e4d54ab570ba8c2107ab89
Saif Ullah <saifullah.feb1088@gmail.com>
2024-06-27 03:26:47 +0500

    Firebase Database Storage for Catgories, make category preview and add routes to different categories

---
+++ src/components/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,19 @@
+import { Fragment, useContext } from "react";
+import { CategoriesContext } from "../../../contexts/categories.context";
+import CategoryPreview from "../../category-preview/category-preview.component";
+import "./categories-preview.styles.scss";
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+  return (
+    <div className="category-preview-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CategoriesPreview;
