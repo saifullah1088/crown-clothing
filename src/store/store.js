d8c82e4707506c150c9b3af9df1c86efd20a13aa
Saif Ullah <saifullah.feb1088@gmail.com>
2024-06-29 23:01:58 +0500

    Redux Persist

--- src/store/store.js
+++ src/store/store.js
@@ -16,7 +16,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export const persistor = persistStore(store);
